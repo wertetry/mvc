de7d58f7b30a7348db9d37a9f0777edb1c2c77fb
wertetry <chenglei0529@163.com>
2020-10-05 13:58:23 +0800

    mvc配置

---
+++ mvc01/src/main/java/com/test/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        req.getSession().setAttribute("msg","执行了"+name+"方法");
+        req.getRequestDispatcher("/hello.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
---
+++ mvc01/web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2020/8/5
+  Time: 23:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  <form action="/hello" method="get">
+    <input name="name"/>
+    <input type="submit"/>
+  </form>
+  </body>
+</html>
